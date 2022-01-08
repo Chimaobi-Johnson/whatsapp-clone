1828aff1721bfbb6455f02ede74a2411460aa9d6
Chimaobi Wogu <woguchimaobi@gmail.com>
2022-01-08 08:13:05 +0100

    created components for chat container and sidebar

---
+++ src/containers/Chat/ChatContainer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from './chatContainer.module.css';
+
+const ChatContainer = props => {
+
+    return (
+        <div className={styles.wrapper}>
+            <h1>CHAT CONTAINER</h1>
+        </div>
+    )
+}
+
+export default ChatContainer
---
+++ src/containers/Sidebar/Sidebar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from './sidebar.module.css';
+
+
+const Sidebar = props => {
+
+    return (
+        <div className={styles.wrapper}>
+            <h1>SIDEBAR</h1>
+        </div>
+    )
+}
+
+export default Sidebar
